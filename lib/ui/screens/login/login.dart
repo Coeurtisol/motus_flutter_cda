677c436653ec6a6a712e0bc88b6fedaf78bb6c7a
dany rose <danyrose1995@gmail.com>
2022-07-11 23:48:09 +0200

    toggle hide password

--- lib/ui/screens/login/login.dart
+++ lib/ui/screens/login/login.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:motus_flutter_cda/ui/screens/login/login_view_model.dart';
 import 'package:provider/provider.dart';
 import 'package:beamer/beamer.dart';
-import 'dart:io';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -12,71 +11,90 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  bool _hidePassword = true;
   TextEditingController loginController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return SafeArea(
+        child: Scaffold(
       backgroundColor: Colors.amber,
-      body: Consumer<LoginViewModel>(builder: ((context, viewModel, child) {
-        viewModel.addListener(() {
-          if (viewModel.player != null) {
-            context.beamToNamed("/home");
-          }
-        });
-        return Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              padding: const EdgeInsets.all(8),
-              child: Column(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.all(8),
-                    child: TextField(
-                        obscureText: false,
-                        controller: loginController,
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: const InputDecoration(
-                            border: OutlineInputBorder(),
+      body: Consumer<LoginViewModel>(
+        builder: ((
+          context,
+          viewModel,
+          child,
+        ) {
+          viewModel.addListener(() {
+            if (viewModel.player != null) {
+              context.beamToNamed("/home");
+            }
+          });
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(8),
+                child: Column(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.all(8),
+                      child: TextField(
+                          controller: loginController,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              fillColor: Colors.white,
+                              filled: true,
+                              labelText: "Login",
+                              labelStyle: TextStyle(
+                                color: Colors.black,
+                                fontSize: 20,
+                              ))),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(8),
+                      child: TextField(
+                        obscureText: _hidePassword,
+                        controller: passwordController,
+                        decoration: InputDecoration(
+                            suffixIcon: InkWell(
+                              onTap: () => setState(() {
+                                _hidePassword = !_hidePassword;
+                              }),
+                              child: Icon(
+                                _hidePassword
+                                    ? Icons.visibility_off
+                                    : Icons.visibility,
+                              ),
+                            ),
+                            border: const OutlineInputBorder(),
                             fillColor: Colors.white,
                             filled: true,
-                            labelText: "Login",
-                            labelStyle: TextStyle(
-                                color: Colors.black,
-                                backgroundColor: Colors.white,
-                                fontSize: 20))),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.all(8),
-                    child: TextField(
-                      obscureText: false,
-                      controller: passwordController,
-                      decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          fillColor: Colors.white,
-                          filled: true,
-                          labelText: "Password",
-                          labelStyle: TextStyle(
+                            labelText: "Password",
+                            labelStyle: const TextStyle(
                               color: Colors.black,
-                              backgroundColor: Colors.white,
-                              fontSize: 20)),
+                              fontSize: 20,
+                            )),
+                      ),
                     ),
-                  ),
-                  ElevatedButton(
+                    ElevatedButton(
                       onPressed: () {
                         viewModel.signIn(
-                            email: loginController.value.text,
-                            password: passwordController.value.text);
+                          email: loginController.value.text,
+                          password: passwordController.value.text,
+                        );
                       },
-                      child: const Text("Se connecter"))
-                ],
-              ),
-            )
-          ],
-        );
-      })),
-    );
+                      child: const Text("Se connecter"),
+                    )
+                  ],
+                ),
+              )
+            ],
+          );
+        }),
+      ),
+    ));
   }
 }
